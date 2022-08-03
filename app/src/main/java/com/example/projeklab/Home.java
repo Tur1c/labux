5caaa546c08ee6b71efd6406b326c0aa92e646df
Turic <ivanderglad200@gmail.com>
2022-08-03 17:44:03 +0700

    image carousel

--- app/src/main/java/com/example/projeklab/Home.java
+++ app/src/main/java/com/example/projeklab/Home.java
@@ -11,9 +11,12 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -36,6 +39,8 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     ViewPagerAdapter adapter;
     private int dotsCount;
     private ImageView[] dots;
+    ImageView iv_btn_1, iv_btn_2, iv_btn_3;
+    Button btnNext, btnPrev;
 
     void init() {
         extras = getIntent().getExtras();
@@ -43,6 +48,13 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         drawerLayout = findViewById(R.id.drawerLayout);
         navView = findViewById(R.id.navView);
         toolbar = findViewById(R.id.toolbar);
+
+        iv_btn_1 = findViewById(R.id.btn_img_1);
+        iv_btn_2 = findViewById(R.id.btn_img_2);
+        iv_btn_3 = findViewById(R.id.btn_img_3);
+
+        btnNext = findViewById(R.id.btn_next_image);
+        btnPrev = findViewById(R.id.btn_prev_image);
 
 
         adapter = new ViewPagerAdapter(this);
@@ -52,38 +64,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         sliderDots = findViewById(R.id.slider_dots);
         dotsCount = adapter.getCount();
         dots = new ImageView[dotsCount];
-
-        for(int i = 0; i < dotsCount; i++) {
-            dots[i] = new ImageView(this);
-            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
-
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(8, 0, 8, 0);
-            sliderDots.addView(dots[i], params);
-        }
-
-        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                for(int i = 0; i < dotsCount; i++) {
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.non_active_dot));
-                }
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
+        dots[0] = iv_btn_1;
+        dots[1] = iv_btn_2;
+        dots[2] = iv_btn_3;
     }
 
     void setUpDrawer() {
@@ -96,6 +79,52 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         tvUsername.setText(extras.getString("username").toString());
     }
 
+    void imageSlider() {
+        for(int i = 0; i < dotsCount; i++) {
+            dots[i].setBackgroundResource(R.drawable.non_active_dot);
+        }
+        dots[0].setBackgroundResource(R.drawable.active_dot);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i = 0; i < dotsCount; i++) {
+                    dots[i].setBackgroundResource(R.drawable.non_active_dot);
+                }
+                dots[position].setBackgroundResource(R.drawable.active_dot);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    void buttonClick() {
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(getItem(+1), true);
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewPager.setCurrentItem(getItem(-1), true);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -103,6 +132,8 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         init();
         changeName();
         setUpDrawer();
+        imageSlider();
+        buttonClick();
     }
 
     @Override
--- app/src/main/java/com/example/projeklab/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/projeklab/adapter/ViewPagerAdapter.java
@@ -44,15 +44,12 @@ public class ViewPagerAdapter extends PagerAdapter {
         ViewPager vp = (ViewPager) container;
         vp.addView(view, 0);
         return view;
-
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-
         ViewPager vp = (ViewPager) container;
         View view = (View) object;
         vp.removeView(view);
-
     }
 }
